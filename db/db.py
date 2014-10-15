d496c8726ed720dfcce72c0f35b126fc6d9dc8a4
David Godfrey <david.godfrey99@gmail.com>
2014-10-16 01:02:07 +0200

    Added calendar.txt gen, todo: trips & stop_times

--- db/db.py
+++ db/db.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
+import pprint
 
 def main():
 
     #agency
     agency = getAgency()
+
+    #dummy calendar
+    calendar = getCalendar()
 
     #stops
     stops = getStops()
@@ -11,10 +15,14 @@ def main():
     #routes
     routes = getRoutes()
 
+    #trips
+    trips = getTrips(routes, calendar)
 
-    #print(agency)
-    print(stops)
-    #print(routes)
+    pp = pprint.PrettyPrinter(indent=4)
+    #pp.pprint(agency)
+    #pp.pprint(stops)
+    #pp.pprint(routes)
+    #pp.pprint(trips)
 
 def getAgency():
     return {
@@ -22,6 +30,63 @@ def getAgency():
         "agency_url": "http://rozklady.mpk.krakow.pl/",
         "agency_timezone": "PL"
     }
+
+def getCalendar():
+    calendar = []
+    id = 1
+    calendar.append({
+        "service_id": "Service" + str(id),
+        "monday": 1,
+        "tuesday": 1,
+        "wednesday": 1,
+        "thursday": 1,
+        "friday": 1,
+        "saturday": 1,
+        "sunday": 1,
+        "start_date": 20000101,
+        "end_date": 20991231
+    })
+    id += 1
+    calendar.append({
+        "service_id": "Service" + str(id),
+        "monday": 1,
+        "tuesday": 1,
+        "wednesday": 1,
+        "thursday": 1,
+        "friday": 1,
+        "saturday": 0,
+        "sunday": 0,
+        "start_date": 20000101,
+        "end_date": 20991231
+    })
+    id += 1
+    calendar.append({
+        "service_id": "Service" + str(id),
+        "monday": 0,
+        "tuesday": 0,
+        "wednesday": 0,
+        "thursday": 0,
+        "friday": 0,
+        "saturday": 1,
+        "sunday": 0,
+        "start_date": 20000101,
+        "end_date": 20991231
+    })
+    id += 1
+    calendar.append({
+        "service_id": "Service" + str(id),
+        "monday": 0,
+        "tuesday": 0,
+        "wednesday": 0,
+        "thursday": 0,
+        "friday": 0,
+        "saturday": 0,
+        "sunday": 1,
+        "start_date": 20000101,
+        "end_date": 20991231
+    })
+
+    return calendar
 
 def getStops():
     stops = []
@@ -35,7 +100,7 @@ def getStops():
             stops.append(
                 { "stop_name": el.text,
                   "href": el.find("a")['href'],
-                  "stop_id": stopID,
+                  "stop_id": "Stop" + str(stopID),
                   "stop_lat": 0,
                   "stop_lon": 0
                 }
@@ -57,6 +122,10 @@ def getRoutes():
         for el in soup.find_all("td"):
             for e in el.find_all("a"):
                 text = e.text
+
+                if not is_number(text):
+                    continue #only numeric routes are allowed
+
                 href = e['href']
                 if tableID <= 3:
                     type = 0 #tram
@@ -64,10 +133,11 @@ def getRoutes():
                     type = 3 #bus
                 routes.append(
                     {
-                        "route_id": routeID,
+                        "route_id": "Route" + str(routeID),
                         "route_short_name": text,
                         "route_long_name": text,
-                        "route_type": type
+                        "route_type": type,
+                        "href": href
                     }
                 )
                 routeID += 1
@@ -75,6 +145,40 @@ def getRoutes():
             tableID += 1
 
     return routes
+
+def getTrips(routes, calendar):
+    trips = []
+
+    url = "rozklady.krakow/aktualne/{:0>4}/{:0>4}w{:0>3}.htm"
+
+    for route in routes:
+        dir = 1
+        id = route['route_short_name']
+        print(route)
+
+        while(True):
+            try:
+                with open(url.format(id, id, dir), encoding="iso-8859-2") as file:
+                    pass
+                    # trips.append({
+                    #     "route_id",: route['route_id'],
+                    #     "service_id":
+                    #
+                    # })
+                # todo
+
+            except FileNotFoundError:
+                #no more directions remaining
+                break
+            break
+        break
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 if __name__ == "__main__":
     main()
