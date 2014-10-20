046321a5debcece3ad11fa6cf0da49b528bbef13
David Godfrey <david.godfrey99@gmail.com>
2014-10-20 23:59:57 +0200

    DB: Parser can now glue geo data to db.pickle created by Crawler. Creates db.json. Should be accessible from c++. Time needed for creation: ~5mins

--- db/parser.py
+++ db/parser.py
@@ -2,8 +2,74 @@ __author__ = 'vka'
 
 import pickle
 import json
+from bs4 import BeautifulSoup
+import pprint
 
 if __name__ == '__main__':
+    pp = pprint.PrettyPrinter(indent=4)
+
     with open('db.pickle', 'rb') as f:
-        p = pickle.load(f)
-        json.dump(p, open('db.json', 'w'))
+        db = pickle.load(f)
+        #json.dump(p, open('db.json', 'w'), indent=2)
+
+    with open('stops.xml', 'r', encoding='utf-8') as f:
+        geo = str(f.readlines())
+        geo = BeautifulSoup(geo)
+
+    markers = geo.find_all('marker')
+    total = len(markers)
+    stats = { 'found': 0, 'nfound': 0 }
+
+    # for m in markers:
+    #     found = False
+    #     for e in db['stops']:
+    #         if e['name'] == m['name']:
+    #             found = True
+    #             stats['found'] += 1
+    #             break
+    #     else:
+    #         stats['nfound'] += 1
+    #
+    #     print(' {: <55}{: >6.8} {: >9.8}{: >5}'.format(m['name'][:53], m['lng'], m['lat'], found))
+    #
+    # print()
+    # print('{: >10}{: >10}{: >10}'.format('Total', 'Found', 'Not found').rjust(79))
+    # print('{: >10}{: >10}{: >10}'.format(total, stats['found'], stats['nfound']).rjust(79))
+
+
+    total = len(markers)
+    stats = { 'found': 0, 'nfound': 0 }
+    nfounds = []
+
+    print(' {: <55}{: >6.8} {: >9.8}{: >5}'.format('Stop name', 'Longtitude', 'Latitude', 'Found'))
+
+    for e in db['stops']:
+        found = False
+
+        for m in markers:
+            if e['name'] == m['name']:
+                found = True
+                stats['found'] += 1
+
+                e['lng'] = m['lng']
+                e['lat'] = m['lat']
+
+                break
+        else:
+            stats['nfound'] += 1
+            nfounds.append(e.copy())
+
+        print(' {: <55}{: >6.8} {: >9.8}{: >5}'.format(m['name'][:53], m['lng'], m['lat'], found))
+
+    print()
+    print('{: >10}{: >10}{: >10}'.format('Total', 'Found', 'Not found').rjust(79))
+    print('{: >10}{: >10}{: >10}'.format(total, stats['found'], stats['nfound']).rjust(79))
+
+    print('\nNot found')
+    for e in nfounds:
+        print(' ' + e['name'])
+
+    with open('db.json', 'w') as f:
+        json.dump(db, f, indent=2)
+
+    print('\nFile saved as db.json')
