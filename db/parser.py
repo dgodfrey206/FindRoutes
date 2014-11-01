3c4fa2d780b004b9fe68d68a7e3070929c99a5c0
David Godfrey <david.godfrey99@gmail.com>
2014-11-01 13:22:00 +0100

    DB: parser work with stop times, still todo

--- db/parser.py
+++ db/parser.py
@@ -5,12 +5,12 @@ import json
 from bs4 import BeautifulSoup
 import pprint
 
-if __name__ == '__main__':
+def parser():
+
     pp = pprint.PrettyPrinter(indent=4)
 
     with open('db.pickle', 'rb') as f:
         db = pickle.load(f)
-        #json.dump(p, open('db.json', 'w'), indent=2)
 
     with open('stops.xml', 'r', encoding='utf-8') as f:
         geo = str(f.readlines())
@@ -20,28 +20,12 @@ if __name__ == '__main__':
     total = len(markers)
     stats = { 'found': 0, 'nfound': 0 }
 
-    # for m in markers:
-    #     found = False
-    #     for e in db['stops']:
-    #         if e['name'] == m['name']:
-    #             found = True
-    #             stats['found'] += 1
-    #             break
-    #     else:
-    #         stats['nfound'] += 1
-    #
-    #     print(' {: <55}{: >6.8} {: >9.8}{: >5}'.format(m['name'][:53], m['lng'], m['lat'], found))
-    #
-    # print()
-    # print('{: >10}{: >10}{: >10}'.format('Total', 'Found', 'Not found').rjust(79))
-    # print('{: >10}{: >10}{: >10}'.format(total, stats['found'], stats['nfound']).rjust(79))
-
 
     total = len(markers)
     stats = { 'found': 0, 'nfound': 0 }
     nfounds = []
 
-    print(' {: <55}{: >6.8} {: >9.8}{: >5}'.format('Stop name', 'Longtitude', 'Latitude', 'Found'))
+    #print(' {: <55}{: >6.8} {: >9.8}{: >5}'.format('Stop name', 'Longtitude', 'Latitude', 'Found'))
 
     for e in db['stops']:
         found = False
@@ -59,17 +43,81 @@ if __name__ == '__main__':
             stats['nfound'] += 1
             nfounds.append(e.copy())
 
-        print(' {: <55}{: >6.8} {: >9.8}{: >5}'.format(m['name'][:53], m['lng'], m['lat'], found))
+    #    print(' {: <55}{: >6.8} {: >9.8}{: >5}'.format(m['name'][:53], m['lng'], m['lat'], found))
 
-    print()
-    print('{: >10}{: >10}{: >10}'.format('Total', 'Found', 'Not found').rjust(79))
-    print('{: >10}{: >10}{: >10}'.format(total, stats['found'], stats['nfound']).rjust(79))
+    #print()
+    #print('{: >10}{: >10}{: >10}'.format('Total', 'Found', 'Not found').rjust(79))
+    #print('{: >10}{: >10}{: >10}'.format(total, stats['found'], stats['nfound']).rjust(79))
 
-    print('\nNot found')
-    for e in nfounds:
-        print(' ' + e['name'])
+    #print('\nNot found')
+    #for e in nfounds:
+    #    print(' ' + e['name'])
 
-    with open('db.json', 'w') as f:
-        json.dump(db, f, indent=2)
+    #with open('db.json', 'w') as f:
+    #    json.dump(db, f, indent=2)
 
-    print('\nFile saved as db.json')
+    #print('\nFile saved as db.json')
+
+    routeID = 0
+    routeTrips = []
+    routeTripsIds = []
+    for t in db['trips']:
+        if t['route_id'] == routeID:
+            routeTrips.append(t.copy())
+            routeTripsIds.append(t['id'])
+
+    #pp.pprint(routeTrips)
+
+    routeStopTimes = []
+
+    for s in db['stop_times']:
+        if s['trip_id'] in routeTripsIds:
+            routeStopTimes.append(s.copy())
+
+    #pp.pprint(routeStopTimes)
+
+    sequences = []
+    seq_id = 0
+    '''
+    sequence:
+        seq_id:
+        trip_id:
+        service_id:
+        seq: [stop_time]
+    '''
+    lastKnownStopId = None
+    lastKnownTripID = None
+    curID = -1
+    for el in routeStopTimes:
+        if lastKnownStopId is not el['stop_id'] or lastKnownTripID is not el['trip_id']:
+            curID = 0
+            lastKnownStopId = el['stop_id']
+            lastKnownTripID = el['trip_id']
+        else:
+            curID += 1
+
+        for t in routeTrips:
+            if t['id'] == el['trip_id']:
+                trip = t.copy()
+                break
+
+        firstStop = el['stop_id'] == t['stop_sec'][0]
+
+        if firstStop:
+            sequences.append({
+                'seq_id': seq_id,
+                'trip_id': trip['id'],
+                'service_id': el['service_id'],
+                'seq': []
+            })
+            seq_id += 1
+        try:
+            sequences[curID]['seq'].append(el['name'])
+        except:
+            pp.pprint(sequences)
+            pp.pprint(el)
+            break
+
+
+if __name__ == '__main__':
+    parser()
