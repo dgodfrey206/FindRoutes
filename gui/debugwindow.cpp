d2b0cb41b8a47ad055bfa69670793801b6db99c3
David Godfrey <david.godfrey99@gmail.com>
2014-11-08 21:43:21 +0100

    Debug window completed.

---
+++ gui/debugwindow.cpp
@@ -0,0 +1,84 @@
+#include "debugwindow.h"
+#include "ui_debugwindow.h"
+
+debugWindow::debugWindow(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::debugWindow)
+{
+    ui->setupUi(this);
+
+    this->file = NULL;
+    this->stream = NULL;
+
+    this->ui->log->setReadOnly(true);
+    this->ui->fileName->setText(QTime::currentTime().toString() + QString(".log"));
+}
+
+debugWindow::~debugWindow()
+{
+    delete ui;
+}
+
+void debugWindow::append(const QString &str)
+{
+    QString msg = this->getCurrentTime() + str;
+    this->ui->log->append(msg);
+    if(this->stream)
+    {
+        (*this->stream) << msg << endl;
+    }
+}
+
+void debugWindow::on_logIntoFile_pressed()
+{
+
+    if(!this->ui->logIntoFile->isChecked()) //unknown behavior as it should work without "!"
+    {
+        if(this->ui->fileName->text().isEmpty())
+        {
+            this->append(QString("No filename specified for logging."));
+            this->ui->logIntoFile->toggle();
+        }
+        else
+        {
+            this->append(QString("Saving into file ") + this->ui->fileName->text());
+            this->file = new QFile(this->ui->fileName->text());
+            this->file->open(QIODevice::WriteOnly);
+            this->stream = new QTextStream(this->file);
+
+            //copy all lines from log into file
+            (*this->stream) << this->ui->log->toPlainText() << endl;
+        }
+    }
+    else
+    {
+        this->append(QString("Saving into file aborted."));
+        //destroy file handle.
+        if(this->stream)
+        {
+            delete this->stream;
+            this->stream = NULL;
+        }
+        if(this->file)
+        {
+            delete this->file;
+            this->file = NULL;
+        }
+    }
+}
+
+void debugWindow::on_clear_pressed()
+{
+    if(this->ui->logIntoFile->isChecked())
+        this->ui->logIntoFile->click();//emulate toogle of log button to get rid of file logging
+    this->ui->log->setText(QString());
+    this->ui->fileName->setText(QTime::currentTime().toString() + QString(".log"));
+    this->append(QString("Log cleared"));
+}
+
+QString debugWindow::getCurrentTime()
+{
+    return QTime::currentTime().toString(QString("hh:mm:ss.zzz: "));
+}
+
+
---
+++ gui/debugwindow.h
@@ -0,0 +1,34 @@
+#ifndef DEBUGWINDOW_H
+#define DEBUGWINDOW_H
+
+#include <QWidget>
+#include <QTime>
+#include <QFile>
+#include <QTextStream>
+
+namespace Ui {
+class debugWindow;
+}
+
+class debugWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit debugWindow(QWidget *parent = 0);
+    ~debugWindow();
+
+    void append(const QString &str);
+
+private:
+    Ui::debugWindow *ui;
+
+    QString getCurrentTime();
+    QFile * file;
+    QTextStream * stream;
+private slots:
+    void on_logIntoFile_pressed();
+    void on_clear_pressed();
+};
+
+#endif // DEBUGWINDOW_H
