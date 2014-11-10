e2696818ece0c2949cfc4d08bfbc96ab2f589827
David Godfrey <david.godfrey99@gmail.com>
2014-11-10 11:45:09 +0100

    class for representing stop times from db

---
+++ src/db/StopTimeData.cpp
@@ -0,0 +1,90 @@
+/*
+ * StopTimeData.cpp
+ *
+ *  Created on: 9 lis 2014
+ *      Author: Rafa³ Prusak
+ */
+
+#include "StopTimeData.h"
+#include <cstdlib>
+
+StopTimeData::StopTimeData(){
+	this->id = 0;
+	this->serviceId = 0;
+	this->stopId = 0;
+	this->tripId = 0;
+}
+
+StopTimeData::StopTimeData(const StopTimeData& src){
+	this->id = src.id;
+	this->serviceId = src.serviceId;
+	this->stopId = src.stopId;
+	this->tripId = src.tripId;
+	this->name = src.name;
+	this->stoptime = src.stoptime;
+}
+
+StopTimeData::StopTimeData(unsigned int id, unsigned int stopId, unsigned int serviceId, unsigned int tripId, std::string name){
+	this->id = id;
+	this->serviceId = serviceId;
+	this->stopId = stopId;
+	this->tripId = tripId;
+	this->name = name;
+	this->stoptime = Time(std::atoi(name.substr(0,2).c_str()), std::atoi(name.substr(3,4).c_str()));
+}
+
+StopTimeData::~StopTimeData(){
+
+}
+
+StopTimeData StopTimeData::operator=(const StopTimeData src){
+	if(this == &src){
+		return *this;
+	}else{
+		this->id = src.id;
+		this->serviceId = src.serviceId;
+		this->stopId = src.stopId;
+		this->tripId = src.tripId;
+		this->name = src.name;
+		this->stoptime = src.stoptime;
+		return *this;
+	}
+}
+
+std::ostream& operator<<(std::ostream& output, const StopTimeData& src){
+	output<<"id="<<src.id<<" serviceId="<<src.serviceId<<" stopId="<<src.stopId<<" tripId="<<src.tripId<<" name="<<src.name<<" time="<<src.stoptime<<std::endl;
+	return output;
+}
+
+bool StopTimeData::operator==(const StopTimeData src){
+	if( (this->id == src.id) && (this->serviceId == src.serviceId) && (this->tripId == src.tripId)
+		&& (this->stopId == src.stopId) && (this->stoptime == src.stoptime)){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+unsigned int StopTimeData::getId() const{
+	return this->id;
+}
+
+unsigned int StopTimeData::getServiceId() const{
+	return this->serviceId;
+}
+
+unsigned int StopTimeData::getStopId() const{
+	return this->stopId;
+}
+
+unsigned int StopTimeData::getTripId() const{
+	return this->tripId;
+}
+
+Time StopTimeData::getStopTime() const{
+	return this->stoptime;
+}
+
+std::string StopTimeData::getName() const{
+	return this->name;
+}
---
+++ src/db/StopTimeData.h
@@ -0,0 +1,42 @@
+/*
+ * StopTimeData.h
+ *
+ *  Created on: 9 lis 2014
+ *      Author: Rafa³ Prusak
+ */
+
+#ifndef DB_STOPTIMEDATA_H_
+#define DB_STOPTIMEDATA_H_
+
+#include <iostream>
+#include <string>
+
+#include "Time.h"
+
+class StopTimeData{
+public:
+		StopTimeData();
+		StopTimeData(const StopTimeData& src);
+		StopTimeData(unsigned int id, unsigned int stopID, unsigned int serviceID, unsigned int tripID, std::string name);
+		~StopTimeData();
+		StopTimeData operator=(const StopTimeData src);
+		friend std::ostream& operator<<(std::ostream& output, const StopTimeData& src);
+		bool operator==(const StopTimeData src);
+		unsigned int getId() const;
+		unsigned int getServiceId() const;
+		unsigned int getStopId() const;
+		unsigned int getTripId() const;
+		Time getStopTime() const;
+		std::string getName() const;
+	private:
+		unsigned int id;
+		unsigned int serviceId;
+		unsigned int stopId;
+		unsigned int tripId;
+		Time stoptime;
+		std::string name;
+};
+
+
+
+#endif /* DB_STOPTIMEDATA_H_ */
