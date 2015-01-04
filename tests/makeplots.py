a1d0996c0f848234e701fa46e4052a2636d58e2e
David Godfrey <david.godfrey99@gmail.com>
2015-01-04 22:35:02 +0100

    Rewritten makeplots script so it can work with subdirs

--- tests/makeplots.py
+++ tests/makeplots.py
@@ -23,34 +23,42 @@ import os
 
 
 if __name__ == '__main__':
-    dir = 'data/'
-    files = os.listdir(dir)
+    directory = 'data/'
+    subdirs = os.listdir(directory)
+    
+    for d in subdirs:
+        curdir = directory + d + '/'
+        files = os.listdir(curdir)
 
-    for f in files:
-        number, test = f.rsplit('.', 1)
+        for f in files:
+            number, test = f.rsplit('.', 1)
 
-        data = {'w': [], 'p': [], 'b': []}
-        with open('data/' + f, 'r') as file:
-            next(file) #skip first line
+            data = {'w': [], 'p': [], 'b': []}
+            with open(curdir + f, 'r') as file:
+                next(file) #skip first line
 
-            for line in file:
-                weight, punishment, best = line.split()
-                data['w'].append(int(weight))
-                data['p'].append(int(punishment))
-                data['b'].append(int(best))
+                for line in file:
+                    weight, punishment, best = line.split()
+                    data['w'].append(int(weight))
+                    data['p'].append(int(punishment))
+                    data['b'].append(int(best))
 
-        time = range(len(data['w']))
-        maximumVal = max([max(data['w']), max(data['b']), max(data['p'])])
+            time = range(len(data['w']))
+            maximumVal = max([max(data['w']), max(data['b']), max(data['p'])])
 
-        plt.plot(time, data['w'], 'r', label='wagi')
-        plt.plot(time, data['p'], 'g', label='kara')
-        plt.plot(time, data['b'], 'b', label='najlepsze')
-        plt.legend()
-        plt.xlabel('Iteracja')
-        plt.ylabel('Wartosci')
-        plt.title(test + ', wartosc: ' + number)
-        plt.axis([0, len(time), 0, maximumVal * 1.2])
+            plt.plot(time, data['w'], 'r', label='wagi')
+            plt.plot(time, data['p'], 'g', label='kara')
+            plt.plot(time, data['b'], 'b', label='najlepsze')
+            plt.legend()
+            plt.xlabel('Iteracja')
+            plt.ylabel('Wartosci')
+            plt.title(test + ', wartosc: ' + number)
+            plt.axis([0, len(time), 0, maximumVal * 1.2])
 
-        plt.savefig('plots/' + f + '.png')
+            savedir = 'plots/' + d + '/'
 
-        plt.clf()
+            if not os.path.exists(savedir):
+                os.makedirs(savedir)
+            plt.savefig(savedir + f + '.png')
+
+            plt.clf()
